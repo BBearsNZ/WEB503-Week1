7a11e7b16694677663ebde3022c36339337ff092
BBearsNZ <brittanycutten@gmail.com>
2022-08-11 10:56:31 +1200

    nav html page

---
+++ src/app.js
@@ -0,0 +1,6 @@
+//adding a delete button to a call back function:
+
+// deleteBtn.addEventListener('click', e => {
+//let id - e.target.parentElement.parentElement.getAttribute('data-id');
+//db.collection('todo').doc(id).delete();
+// });
