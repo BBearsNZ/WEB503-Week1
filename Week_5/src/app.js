381f71d18ab371a53b84cd946329872600cb9b00
BBearsNZ <brittanycutten@gmail.com>
2022-08-25 09:01:27 +1200

    week 5 commits

--- Week_5/src/app.js
+++ Week_5/src/app.js
@@ -1,1 +1,44 @@
+//add button
+const addBtn = document.getElementById('add');
 
+//Click event listener
+
+addBtn.addEventListener('click', ()=> addNewNote);
+
+function addNewNote(text=  '') {
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
+        <div class="actions">   
+            <button class="edit">
+                <i class="fas fa-edit"></i>
+            </button>
+
+            <button class="delete">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+        </div>
+
+        <div class=`main ${text ? " : "hidden"}`>
+        <textarea class=`${text ? " : "hidden"}`>${text}</textarea>
+    `
+    const editBtn = note.querySelector('.edit');
+    const main = note.querySelector('.main');
+    const textarea = note.querySelector('textarea');
+    deleteBtn.addEventListener('click', ()=> {
+        note.remove();
+    } )
+    editBtn.addEventListener('click', ()=> {
+        main.classList.toggle('hidden');
+        textarea.classList.toggle('hidden');    
+    } )
+
+    textArea.addEventListener('inout', (e) => {
+        const {value} = e.target;
+
+        main.innerHTML = marked(value);         
+    })
+
+    document.body.appendChild(note);
+    }
